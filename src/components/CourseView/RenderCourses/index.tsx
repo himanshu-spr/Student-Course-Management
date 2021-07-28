66e49005a44cf7de078a65bcbc3b6646f775504c
Himanshu Chauhan <himanshu.chauhan@sprinklr.com>
2021-07-28 20:59:14 +0530

    removed unnecessary useCallback usage

--- src/components/CourseView/RenderCourses/index.tsx
+++ src/components/CourseView/RenderCourses/index.tsx
@@ -1,21 +1,20 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { RenderCoursesProps, Course } from "../../../interfaces";
 import ListCourse from "../CourseListView/ListCourse";
 import GridCourse from "../CourseGridView/GridCourse";
 
 const RenderCourses = (props: RenderCoursesProps) => {
-  const coursesElement = useCallback(
-    () =>
-      props.courses.map((course: Course) => {
+  return (
+    <>
+      {props.courses.map((course: Course) => {
         return props.view === "List" ? (
           <ListCourse course={course} />
         ) : (
           <GridCourse course={course} />
         );
-      }),
-    [props]
+      })}
+    </>
   );
-  return <>{coursesElement()}</>;
 };
 
 export default RenderCourses;
